83572a52aa6c1dcdc09980d5387308284e4be5e6
aiya000 <aiya000.develop@gmail.com>
2018-03-23 23:21:14 +0900

    :+1: Remove an under gap of layoutHook

--- XMonadConfig/LayoutHook.hs
+++ XMonadConfig/LayoutHook.hs
@@ -7,14 +7,12 @@ module XMonadConfig.LayoutHook
 import XMonad (Window)
 import XMonad.Layout ((|||), Choose, Full(..))
 import XMonad.Layout.Decoration (Decoration, DefaultShrinker)
-import XMonad.Layout.Gaps (Gaps, gaps)
 import XMonad.Layout.Grid (Grid(..))
 import XMonad.Layout.LayoutModifier (ModifiedLayout)
 import XMonad.Layout.Simplest (Simplest)
 import XMonad.Layout.SubLayouts (subTabbed, Sublayout)
 import XMonad.Layout.Tabbed (TabbedDecoration)
 import XMonad.Layout.TwoPane (TwoPane(..))
-import XMonad.Util.Types (Direction2D(..))
 
 
 infixr 1 :$
@@ -26,14 +24,12 @@ infixr 2 :|||
 type (:|||) = Choose
 
 
-type MyLayoutHook  = (TaskbarMargin :$ TwoTabbedPane :||| Grid) :||| Full
-type TaskbarMargin = Gaps
+type MyLayoutHook  = TwoTabbedPane :||| Grid :||| Full
 type TwoTabbedPane = SubTabbed TwoPane
 type SubTabbed x   = (Decoration TabbedDecoration DefaultShrinker :. Sublayout Simplest) x
 
 
 myLayoutHook :: MyLayoutHook Window
-myLayoutHook = (taskbarMargin $ twoTabbedPane ||| Grid) ||| Full
+myLayoutHook = twoTabbedPane ||| Grid ||| Full
   where
-    taskbarMargin = gaps [(D, 40)]
     twoTabbedPane = subTabbed $ TwoPane (1/55) (1/2)
