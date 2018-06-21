a3081ad9d6c95851685309eaacc9d675d8d13b28
aiya000 <aiya000.develop@gmail.com>
2018-06-21 16:36:34 +0900

    :+1: Report a log for replace.sh

--- replace.sh
+++ replace.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-stack install \
-    && stack exec -- xmonad-config --recompile \
-    && stack exec -- xmonad-config --restart \
-    && killall xmonad-x86_64-linux \
-    && xmonad-config > /dev/null 2>&1 &
+# shellcheck disable=SC2088
+cd ~/.xmonad || (notify-send '~/.xmonad is not found' ; exit 1)
+{
+    echo '.xmonad/replace.sh starts'
+    stack install
+    stack exec -- xmonad-config --recompile
+    stack exec -- xmonad-config --restart
+    killall xmonad-x86_64-linux
+    stack exec -- xmonad-config > /dev/null 2>&1 &
+} >> ~/.xmonad/xmonad-config.log 2>&1
