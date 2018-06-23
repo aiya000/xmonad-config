5d27102be823fc2fab554c2cfe76c6e15cc101fe
aiya000 <aiya000.develop@gmail.com>
2018-06-23 13:58:47 +0900

    :+1: Fix replace.sh not to continue if failed

--- replace.sh
+++ replace.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2088
 cd ~/.xmonad || (notify-send '~/.xmonad is not found' ; exit 1)
 {
-    echo '.xmonad/replace.sh starts'
-    stack install
-    stack exec -- xmonad-config --recompile
-    stack exec -- xmonad-config --restart
-    killall xmonad-x86_64-linux
+    echo '.xmonad/replace.sh starts' &&
+    stack install &&
+    stack exec -- xmonad-config --recompile &&
+    stack exec -- xmonad-config --restart &&
+    killall xmonad-x86_64-linux &&
     stack exec -- xmonad-config > /dev/null 2>&1 &
 } >> ~/.xmonad/xmonad-config.log 2>&1
