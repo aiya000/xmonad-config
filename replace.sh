4870be1699acee95ec1fbf9c97745a5e8924a14f
aiya000 <aiya000.develop@gmail.com>
2018-08-22 19:49:51 +0900

    replace.sh

--- replace.sh
+++ replace.sh
@@ -6,9 +6,10 @@ cd "$xmonad_dir" || (notify-send "$xmonad_dir is not found" ; exit 1)
 
 {
     echo "$xmonad_dir/replace.sh starts" &&
+    stack clean &&
     stack install &&
     stack exec -- xmonad-config --recompile &&
     stack exec -- xmonad-config --restart &&
     killall xmonad-x86_64-linux &&
     stack exec -- xmonad-config > /dev/null 2>&1 &
-} >> "$xmonad_dir/xmonad-config.log" 2>&1
+} | tee -a "$xmonad_dir/xmonad-config.log" 2>&1
