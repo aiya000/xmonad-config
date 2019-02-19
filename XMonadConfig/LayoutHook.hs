a9b9f01b1f01b16c0e225ce85754f776b368aad1
aiya000 <aiya000.develop@gmail.com>
2019-02-19 17:41:42 +0900

    :recycle: Use the partial type signature for the layout

--- XMonadConfig/LayoutHook.hs
+++ XMonadConfig/LayoutHook.hs
@@ -1,34 +1,20 @@
+{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
+
 {-# LANGUAGE TypeOperators #-}
 
 module XMonadConfig.LayoutHook
   ( myLayoutHook
   ) where
 
-import XMonad (Window, Mirror(..))
-import XMonad.Hooks.ManageDocks (AvoidStruts, avoidStruts)
-import XMonad.Layout (Choose, Full (..), (|||))
-import XMonad.Layout.Decoration (Decoration, DefaultShrinker)
+import XMonad (Window)
+import XMonad.Hooks.ManageDocks (avoidStruts)
+import XMonad.Layout (Full (..), (|||))
 import XMonad.Layout.Grid (Grid (..))
-import XMonad.Layout.LayoutModifier (ModifiedLayout)
-import XMonad.Layout.Simplest (Simplest)
 import XMonad.Layout.StackTile (StackTile (..))
-import XMonad.Layout.SubLayouts (Sublayout, subTabbed)
-import XMonad.Layout.Tabbed (TabbedDecoration)
+import XMonad.Layout.SubLayouts (subTabbed)
 import XMonad.Layout.TwoPane (TwoPane (..))
 
-myLayoutHook :: ModifiedLayout
-                        AvoidStruts
-                        (Choose
-                           (ModifiedLayout
-                              (Decoration TabbedDecoration DefaultShrinker)
-                              (ModifiedLayout (Sublayout Simplest) TwoPane))
-                           (Choose StackTile (Choose Grid (Choose (Mirror StackTile) Full))))
-                        Window
-myLayoutHook = avoidStruts $
-  twoTabbedPane ||| stack ||| Grid ||| gimp ||| Full
+myLayoutHook :: _ Window
+myLayoutHook = avoidStruts $ twoTabbedPane ||| Full
   where
-    nanika = 3 / 200
-
-    stack = StackTile 1 nanika (1 / 2)
     twoTabbedPane = subTabbed $ TwoPane (1 / 55) (1 / 2)
-    gimp = Mirror $ StackTile 1 nanika (1 / 6)
