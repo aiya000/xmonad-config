06c8e6e7524ecf01b708fa86f1ecbf4df19940b3
aiya000 <aiya000.develop@gmail.com>
2020-11-10 16:58:50 +0900

    :+1: Restore the 1st workspace

--- XMonadConfig/Keys.hs
+++ XMonadConfig/Keys.hs
@@ -65,15 +65,14 @@ myKeys fingers _ = M.fromList $
       , ((shiftMask, xK_Print), spawn "xfce4-screenshooter")
       ]
 
-    -- NOTE: 1 is hard to type.
     switchingWorkspaces = do
-        (numKey, workspace) <- zip [xK_2 .. xK_9] myWorkspaces
+        (numKey, workspace) <- zip [xK_1 .. xK_9] myWorkspaces
         let keymap = (thumbMask .|. littleMask, numKey)
         let switching = windows $ greedyView workspace
         pure (keymap, switching)
 
     puttingWindowsToWorkspace = do
-      (numKey, workspace) <- zip [xK_2 .. xK_9] myWorkspaces
+      (numKey, workspace) <- zip [xK_1 .. xK_9] myWorkspaces
       let keymap = (ringMask, numKey)
       let putting = windows $ shift workspace
       pure (keymap, putting)
